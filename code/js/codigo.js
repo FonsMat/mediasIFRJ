27c534358f7a8bb87724de3cb5f87d1e73d2d632
FonsMat123 <matheus.fonsecak@gmail.com>
2020-03-12 16:35:41 -0300

    Comentarios adicionados e números formatados

--- code/js/codigo.js
+++ code/js/codigo.js
@@ -12,71 +12,99 @@ function btnclick(){
     var g2 = Number(lbl3.value)
     var rec2 = Number(lbl4.value)
     
-    
-    if (g1 < 6){
-        if (rec1 == 0){
+    //tem alguns comentarios que parecem inuteis, mas vão ajudar a entender o codigo!
+
+    if (g1 < 6){ 
+        //Se a pessoa ficar de rec no g1
+
+        if (rec1 == 0){ 
+            //Se a pessoa não colocou a recuperação
             var frec1 = (30 - 2 * g1) / 3
-            res.innerText = `Você precisa tirar ${frec1} na recuperaçâo para ficar com 6 na média do G1!`
+            res.innerText = `Você precisa tirar ${frec1.toFixed(1)} na recuperaçâo para ficar com 6 na média do G1!`
+
         }
-        else {
+        else { 
+            //agora vai mostrar como ficou a média do g1 com a recuperação e informações do g2
             var g1rec1 = (2 * g1 + 3 * rec1) / 5
-            if (g2 == 0){
+
+            if (g2 == 0){ 
+                //se a pessoa não colocou o g2
                 var fg2 = (18 - g1) / 2
-                res.innerText = `Sua média no G1 é de ${g1rec1} e você precisa tirar ${fg2} no G2 para passar!`
+                res.innerText = `Sua média no G1 é de ${g1rec1.toFixed(1)} e você precisa tirar ${fg2.toFixed(1)} no G2 para passar!`
+
             }
             else if (g2 < 6){
+                //se a pessoa ficar de rec no g2
+
                 if (rec2 == 0) {
+                    //se a pessoa não colocou a rec do g2
                     var frec2 = (30 - 2 * g2) / 3
-                    res.innerText = `Sua média no G1 é de ${g1rec1} e você precisa tirar ${frec2} na recuperação do G2 pra passar!`
+                    res.innerText = `Sua média no G1 é de ${g1rec1.toFixed} e você precisa tirar ${frec2.toFixed(1)} na recuperação do G2 pra passar!`
+
                 }
-                else {
+                else { 
+                    //isso vai acontecer se a pessoa ficar de rec os dois bimestres e passar
                     var g2rec2 = (2 * g1 + 3 * rec1) / 5
                     var mfrec1rec2 = (g1rec1 + 2 * g2rec2) / 3
+
                     if (mfrec1rec2 < 6) {
-                        res.innerText = `Sua média final é ${mfrec1rec2}, você não passou, boa sorte no próximo periodo!`
+                        res.innerText = `Sua média final é ${mfrec1rec2.toFixed(1)}! Você não passou, boa sorte no próximo periodo!`
+
                     }
                     else {
-                        res.innerText = `Sua média final é ${mfrec1rec2}, você passou, parabens!`
+                        res.innerText = `Sua média final é ${mfrec1rec2.toFixed(1)}! Você passou, parabens!`
+
                     }
                 }
             }
             else {
+                //isso vai acontecer se a pessoa ficar de rec os dois bimestres e não passar
                 var mfrec1 = (g1rec1 + 2 * g2) / 3
+
                 if (mfrec1 < 6) {
-                    res.innerText = `Sua média final é ${mfrec1}, você não passou, boa sorte no próximo periodo!`
+                    res.innerText = `Sua média final é ${mfrec1.toFixed(1)}! Você não passou, boa sorte no próximo periodo!`
+
                 }
                 else {
-                    res.innerText = `Sua média final é ${mfrec1}, você passou, parabens!`
+                    res.innerText = `Sua média final é ${mfrec1.toFixed(1)}! Você passou, parabens!`
+
                 }
             }
         }
     } else if (g1 >= 6 && g2 == 0){
+        //se a pessoa passou no g1 e deixou o g2 em branco
         var mg2 = (18 - g1) / 2
-        res.innerText = `Você precisa tirar ${mg2} no G2 para passar!`
+        res.innerText = `Você precisa tirar ${mg2.toFixed(1)} no G2 para passar!`
+
     }
     else {
         if (g2 >= 6){
+            //se a pessoa passou direto
             mf = (g1 + 2 * g2) / 3
-            if (mf < 6){
-                res.innerText = `Sua média final é ${mf}, você não passou, boa sorte no próximo periodo!`
-            }
-            else {
-                res.innerText = `Sua média final é ${mf}, você passou, parabens!`
-            }
+            res.innerText = `Sua média final é ${mf.toFixed(1)}, você passou, parabens!`
+            
         }
         else {
+            //se a pessoa ficou de rec no g2
+
             if (rec2 == 0) {
+                //se a pessoa não digitou a rec do g2 
                 var frec2 = (30 - 2 * g2) / 3
-                res.innerText = `Você precisa tirar ${frec2} na recuperação do G2 pra passar!`
+                res.innerText = `Você precisa tirar ${frec2.toFixed(1)} na recuperação do G2 pra passar!`
+
             }
             else {
+                //o que vai acontecer se a pessoa ficou de rec no g2
                 var g2rec2 = (2 * g1 + 3 * rec2) / 5
                 var mfrec2 = (g1 + 2 * g2rec2) / 3
+
                 if (mfrec2 < 6) {
-                    res.innerText = `Sua média final é ${mfrec2}, você não passou, boa sorte no próximo periodo!`
+                    res.innerText = `Sua média final é ${mfrec2.toFixed(1)}, você não passou, boa sorte no próximo periodo!`
+                    
                 }
                 else {
-                    res.innerText = `Sua média final é ${mfrec2}, você passou, parabens!`
+                    res.innerText = `Sua média final é ${mfrec2.toFixed(1)}, você passou, parabens!`
+
                 }
             }
         }
