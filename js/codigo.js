73f332ef0a4c3ce71f13231d1e4aefe2ee6e72a1
FonsMat123 <matheus.fonsecak@gmail.com>
2020-03-14 15:20:20 -0300

    Correção de bugs

--- js/codigo.js
+++ js/codigo.js
@@ -6,14 +6,14 @@ const lblr1 = window.document.getElementById('lblr1');
 const lblg2 = window.document.getElementById('lblg2');
 const lblr2 = window.document.getElementById('lblr2');
 const res = window.document.getElementById('res');
-const erro = window.document.getElementById('error')
 
 
 function btnclick(){
 
     if (lblg1.value.length == 0){
 
-        erro.innerText = '[ERROR] FALTAM DADOS!';
+        res.innerText = '[ERROR] FALTAM DADOS!';
+        res.style.color = "red"
 
     }
     else{
@@ -22,7 +22,8 @@ function btnclick(){
 
         if (g1 < 0 || g1 > 10){
 
-            erro.innerText = '[ERROR] NOTA IMPOSSÍVEL!!!!!';
+            res.innerText = '[ERROR] NOTA IMPOSSÍVEL!!!!!';
+            res.style.color = "red"
 
         }
         else if (g1 >= 6){
@@ -32,6 +33,8 @@ function btnclick(){
 
                 const g2v = (18 - g1) / 2;
                 res.innerText = `Você precisa tirar ${g2v.toFixed(1)} no G2 para passar!`;
+                res.style.color =  "black"
+    
 
             }
             else {
@@ -40,7 +43,8 @@ function btnclick(){
 
                 if (g2 < 0 || g2 > 10){
 
-                    erro.innerText = '[ERROR] NOTA IMPOSSÍVEL!!!!!';
+                    res.innerText = '[ERROR] NOTA IMPOSSÍVEL!!!!!';
+                    res.style.color = "red"
 
                 }
                 else if (g2 >= 6){
@@ -48,6 +52,8 @@ function btnclick(){
 
                     mf = (g1 + 2 * g2) / 3;
                     res.innerText = `Sua média final é ${mf.toFixed(1)}, você passou, parabens!`;
+                    res.style.color =  "black"
+    
 
                 }
                 else {
@@ -56,6 +62,7 @@ function btnclick(){
 
                         const frec2 = (30 - 2 * g2) / 3;
                         res.innerText = `Você precisa tirar ${frec2.toFixed(1)} na recuperação do G2 pra passar!`;
+                        res.style.color =  "black"
 
                     }
                     else {
@@ -68,11 +75,13 @@ function btnclick(){
                         if (mf >= 6){
 
                             res.innerText = `Sua média do G2 é ${mg2.toFixed(1)} e sua média final é ${mf.toFixed(1)}!! Você passou, parabens!!!`;
+                            res.style.color =  "black"
 
                         }
                         else {
 
                             res.innerText = `Sua média do G2 é ${mg2.toFixed(1)} e sua média final é ${mf.toFixed(1)}!!  Você não passou, boa sorte no proximo periodo!`;
+                            res.style.color =  "black"
                             
                         }
                     }
@@ -82,17 +91,22 @@ function btnclick(){
         else if (g1 < 6){
             //se o aluno ficou de rec no g1
             if(lblr1.value.length == 0){
+
                 const rec1 = (30 - 2 * g1) / 3
                 res.innerText = `Você precisa tirar ${rec1.toFixed(1)} na recuperaçâo para ficar com 6 na média do G1!`
+                res.style.color =  "black"
+
             }
             else{
+
                 const nrec1 = Number(lblr1.value)
                 const rec1 = (2 * g1 + 3 * nrec1) / 5
+
                 if (lblg2.value.length == 0){
 
                     const g2v = (18 - rec1) / 2;
                     res.innerText = `Sua média do g1 é ${rec1.toFixed(1)} e você precisa tirar ${g2v.toFixed(1)} no G2 para passar!`;
-    
+                    res.style.color =  "black"
                 }
                 else {
     
@@ -100,7 +114,8 @@ function btnclick(){
     
                     if (g2 < 0 || g2 > 10){
     
-                        erro.innerText = '[ERROR] NOTA IMPOSSÍVEL!!!!!';
+                        res.innerText = '[ERROR] NOTA IMPOSSÍVEL!!!!!';
+                        res.style.color =  "red"
     
                     }
                     else if (g2 >= 6){
@@ -108,7 +123,8 @@ function btnclick(){
     
                         mf = (rec1 + 2 * g2) / 3;
                         res.innerText = `Sua média do g1 é ${rec1.toFixed(1)} e final é ${mf.toFixed(1)}, você passou, parabens!`;
-    
+                        res.style.color =  "black"
+
                     }
                     else {
     
@@ -116,7 +132,8 @@ function btnclick(){
     
                             const frec2 = (30 - 2 * g2) / 3;
                             res.innerText = `Sua média do g1 é ${rec1.toFixed(1)} e você precisa tirar ${frec2.toFixed(1)} na recuperação do G2 pra passar!`;
-    
+                            res.style.color =  "black"
+
                         }
                         else {
                             //se o aluno ficou de rec no g2
@@ -128,11 +145,13 @@ function btnclick(){
                             if (mf >= 6){
     
                                 res.innerText = `Sua média do g1 é ${rec1.toFixed(1)} e sua média do G2 é ${mg2.toFixed(1)} e sua média final é ${mf.toFixed(1)}!! Você passou, parabens!!!`;
-    
+                                res.style.color =  "black"
                             }
                             else {
     
                                 res.innerText = `Sua média do g1 é ${rec1.toFixed(1)} e sua média do G2 é ${mg2.toFixed(1)} e sua média final é ${mf.toFixed(1)}!!  Você não passou, boa sorte no proximo periodo!`;
+                                res.style.color =  "black"
+                                
                             }
                         }
                     }
